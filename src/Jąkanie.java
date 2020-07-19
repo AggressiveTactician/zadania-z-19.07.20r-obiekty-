96f9325204f0524f6ed15a10a37fcad740ab7dfc
Użytkownik <uytkownik@kursGIT.pl>
2020-07-19 12:17:00 +0200

    zad13 rozwiązane

---
+++ src/Jąkanie.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Jąkanie {
+    private String pobierz() {
+        Scanner input = new Scanner(System.in);
+        return input.nextLine();
+    }
+    private String [] split(String text) {
+        return text.split(" ");
+    }
+    private void wypisz(String[] text) {
+        StringBuilder sb = new StringBuilder();
+        for (String word : text) {
+            sb.append(word)
+                    .append(" ")
+                    .append(word)
+                    .append(" ");
+        }
+        System.out.println(sb.toString());
+    }
+    public void run(){
+        wypisz(split(pobierz()));
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,11 @@
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	// Napisz program, który „się jąka”, to znaczy pobiera użytkownika tekst (zmienną typu String),
+        // a następnie wypisuje podany tekst, w którym każde słowo wypisane jest po dwa razy.
+        // Przykładowo, dla wejścia: „To jest mój test” program powinien wypisać „To To jest jest mój mój test test”.
+
+        Jąkanie jąkanie = new Jąkanie();
+        jąkanie.run();
     }
 }
